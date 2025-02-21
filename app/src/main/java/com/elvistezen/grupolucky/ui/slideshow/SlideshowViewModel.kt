26145d391dd01bcb99cb95e84f29be3be95a1f4f
Elvis Tezen <elvistezenv@gmail.com>
2025-02-21 12:06:30 -0500

    enviado final

--- app/src/main/java/com/elvistezen/grupolucky/ui/slideshow/SlideshowViewModel.kt
+++ app/src/main/java/com/elvistezen/grupolucky/ui/slideshow/SlideshowViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class SlideshowViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "Hola grupo lucky. Me faltó tiempo para cerrar la sesión. :("
     }
     val text: LiveData<String> = _text
 }
