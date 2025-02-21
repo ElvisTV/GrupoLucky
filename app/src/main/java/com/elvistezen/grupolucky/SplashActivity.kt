7fb67e26be443b67951adfebeb9b82d678f3074c
Elvis Tezen <elvistezenv@gmail.com>
2025-02-21 00:16:14 -0500

    subidita el splash screen con video

---
+++ app/src/main/java/com/elvistezen/grupolucky/SplashActivity.kt
@@ -0,0 +1,33 @@
+package com.elvistezen.grupolucky
+
+import android.content.Intent
+import android.media.MediaPlayer
+import android.net.Uri
+import android.os.Bundle
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val videoView: VideoView = findViewById(R.id.splashVideoView)
+        val videoUri = Uri.parse("android.resource://" + packageName + "/" + R.raw.fondocielo)
+        videoView.setVideoURI(videoUri)
+        videoView.setOnCompletionListener {
+            navigateToMain()
+        }
+
+        // Reproducir sonido
+        val mediaPlayer = MediaPlayer.create(this, R.raw.fondocielo)
+        mediaPlayer.start()
+
+        videoView.start()
+    }
+
+    private fun navigateToMain() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
